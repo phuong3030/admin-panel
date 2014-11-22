87f1cf7eb374ff53d08410479ec0ee989ffad6fc
sir1001dem <sir1001dem@gmail.com>
2014-11-23 01:41:56 +0700

    Feature - Taxonomy tree: modify nestable library (don't permit to move taxonomy to taxon's position and vice versa)

--- app/assets/javascripts/admin/taxonomy/index.js
+++ app/assets/javascripts/admin/taxonomy/index.js
@@ -1,6 +1,3 @@
 //= require nestable.js 
 
-$('.dd').nestable({
-  expandBtnHTML: '<button data-action="expand"><i class="fa fa-caret-left"></i></button>',
-  collapseBtnHTML: '<button data-action="collapse"><i class="fa fa-caret-down"></i></button>'
-});
+$('.dd').nestable();
