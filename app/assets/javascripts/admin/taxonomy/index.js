3ec19125693ea674319dc71c7749eab23a004aa9
sir1001dem <sir1001dem@gmail.com>
2014-11-22 18:16:16 +0700

    Feature - javascript, css included

---
+++ app/assets/javascripts/admin/taxonomy/index.js
@@ -0,0 +1,6 @@
+//= require nestable.js 
+
+$('.dd').nestable({
+  expandBtnHTML: '<button data-action="expand"><i class="fa fa-caret-left"></i></button>',
+  collapseBtnHTML: '<button data-action="collapse"><i class="fa fa-caret-down"></i></button>'
+});
