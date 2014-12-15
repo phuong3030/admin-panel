a252f1cbc0d1ee7b41b81b88b6cfe2ed3b267500
sir1001dem <sir1001dem@gmail.com>
2014-12-15 13:38:47 +0700

    Configure - content show for cms page

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     comfy_route :cms_admin, :path => '/admin/cms'
 
     # Root for render page from CMS system
-    root :to => 'cms/content#show'
+    root :to => 'comfy/cms/content#show'
 
     # Make sure this routeset is defined last
     comfy_route :cms, :path => '/', :sitemap => false
