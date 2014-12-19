19deeaf85b7daae97e94b8b79c36090fe5289bdc
sir1001dem <sir1001dem@gmail.com>
2014-12-19 21:25:47 +0700

    Features - jasmine specRunner is added; boot.js (init jasmine) is added; trying to write first jasmine test in project

--- app/assets/javascripts/admin/test/specs/spec.js
+++ app/assets/javascripts/admin/test/specs/spec.js
@@ -1,15 +1,13 @@
 // Jasmine Unit Testing Suite
-define(["jquery", "backbone", "marionette", "models/Model", "collections/Collection", "views/WelcomeView","views/DesktopHeaderView","views/MobileHeaderView"],
-    function($, Backbone, Marionette, Model, Collection, WelcomeView, DesktopHeaderView, MobileHeaderView) {
+define(["jquery", "backbone", "marionette", "models/user", "collections/Collection"],
+    function($, Backbone, Marionette, User, Collection) {
         // Test suite that includes all of the Jasmine unit tests
         describe("Marionette-Require-Boilerplate (MRB)", function() {
-            /*// Marionette App Suite: contains all tests related to views
+            // Marionette App Suite: contains all tests related to views
             describe("Desktop Marionette App instantiation", function() {
                 //Initialize App in Desktop Mode (App is global var)
                 App.start();
                 it("App should start and have Regions", function() {
-                    expect(App.mainRegion.el).toEqual("#main");
-                    expect(App.headerRegion.el).toEqual("header");
                 });
 
             }); // End of the App test suite*/
@@ -17,15 +15,6 @@ define(["jquery", "backbone", "marionette", "models/Model", "collections/Collect
             // Marionette View Suite: contains all tests related to views
             describe("Marionette Views", function() {
                 it("Instantiate some Views", function() {
-                    var container = $("#container");
-                    var welcomeView = new WelcomeView();
-                    container.append(welcomeView.render().$el);
-
-                    var desktopHeaderView = new DesktopHeaderView();
-                    container.append(desktopHeaderView.render().$el);
-
-                    var mobileHeaderView = new MobileHeaderView();
-                    container.append(mobileHeaderView.render().$el);
                 });
             }); // End of the View test suite*/
 
@@ -35,19 +24,19 @@ define(["jquery", "backbone", "marionette", "models/Model", "collections/Collect
                 // Runs before every Model spec
                 beforeEach(function() {
                     // Instantiates a new Model instance
-                    this.model = new Model();
+                    this.user = new User();
 
                     // We are spying on the _validate method to see if it gets called
-                    spyOn(Model.prototype, "validate").andCallThrough();
+                    spyOn(User.prototype, "validate").andCallThrough();
                 });
 
                 it("should be in a valid state", function() {
-                    expect(this.model.isValid()).toBe(true);
+                    expect(this.user.isValid()).toBe(true);
                 });
 
                 it("should call the validate method when setting a property", function() {
-                    this.model.set({ example: "test" }, { validate: true });
-                    expect(Model.prototype.validate).toHaveBeenCalled();
+                    this.user.set({ example: "test" }, { validate: true });
+                    expect(User.prototype.validate).toHaveBeenCalled();
                 });
 
             }); // End of the Model test suite
