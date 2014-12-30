2a0aa48d4e89597a31b923bfb00b7af47cbf1def
sir1001dem <sir1001dem@gmail.com>
2014-12-30 18:26:53 +0700

    panel header view spec added

--- app/assets/javascripts/admin/test/specs/views/panel_header-view_spec.js
+++ app/assets/javascripts/admin/test/specs/views/panel_header-view_spec.js
@@ -4,13 +4,30 @@ define(
     'backbone', 
     'marionette', 
     'jquery', 
-    'hbs!templates/shared/header/panel_header'
+    'views/shared/header/panel_header-view'
   ],
-  function (App, Backbone, Marionette, $, panelHeaderTemplate) {
+  function (App, Backbone, Marionette, $, PanelHeader) {
 
     describe('Resize left sidebar', function () {
+
+      var panelHeader = new PanelHeader();
+
+      beforeEach(function () {
+
+        panelHeader = new PanelHeader();
+      });
+
+      it('should have nav-medium-button', function () {
+
+        //expect($('.panel-menu')).toContain($('.nav-medium-button'));
+      });
+
+      it('should have nav-small-button', function () {
+
+        //expect($('.nav-small-button')).toBeInDom();
+      });
+
       it('should be send resize command to application'); 
     });
-
   }
 );
