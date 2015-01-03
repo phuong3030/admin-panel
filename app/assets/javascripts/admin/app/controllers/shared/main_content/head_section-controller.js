39db59a75ab8281029f0b174089cee6ae5dbb811
sir1001dem <sir1001dem@gmail.com>
2015-01-03 11:06:04 +0700

    Head section controller & spec created

---
+++ app/assets/javascripts/admin/app/controllers/shared/main_content/head_section-controller.js
@@ -0,0 +1,8 @@
+define(
+  ['app', 'views/shared/main_content/head_section-view'],
+  function (App, HeadSectionView) {
+    return Backbone.Marionette.Controller.extend({
+    
+    });
+  }
+);
---
+++ app/assets/javascripts/admin/test/specs/controllers/shared/main_content/head_section-controller_spec.js
@@ -0,0 +1,10 @@
+define(
+  ['app', 'controllers/shared/main_content/head_section-controller'],
+  function (App, HeadSectionController) {
+
+    describe('Head section controller', function () {
+    
+      it('should call head section view to re-render new breadcrumb when hash changed'); 
+    });
+  }
+);
