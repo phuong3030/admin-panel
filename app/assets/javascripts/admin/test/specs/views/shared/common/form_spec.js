4a6fddaf2e193b7952ea7ce22db48d6a0ff4b1bd
sir1001dem <sir1001dem@gmail.com>
2015-01-09 09:57:02 +0700

    form spec added; handlebars form helper added and modified

---
+++ app/assets/javascripts/admin/test/specs/views/shared/common/form_spec.js
@@ -0,0 +1,10 @@
+define(['views/shared/common/form'], function (FormView) {
+
+  describe('Form template view', function () {
+  
+    it('should have _method attribute', function () {
+    });
+
+
+  });
+});
