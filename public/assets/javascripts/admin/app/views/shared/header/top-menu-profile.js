b1e6eb1d11c0f98e8e5e3c9297463df319c0694f
sir1001dem <sir1001dem@gmail.com>
2015-03-05 09:05:43 +0700

    Child menu added

--- public/assets/javascripts/admin/app/views/shared/header/top-menu-profile.js
+++ public/assets/javascripts/admin/app/views/shared/header/top-menu-profile.js
@@ -1,12 +1,37 @@
 define(
   [
     'app',
+    'models/user',
     'hbs!templates/shared/header/top-menu-profile'
-  ], function (App, topMenuProfileTemplate) {
+  ], function (App, User, topMenuProfileTemplate) {
     
-    return Backbone.Marionette.LayoutView.extend({
+    return Backbone.Marionette.ItemView.extend({
       template: topMenuProfileTemplate,
-      className: 'admin_mega-menu pull-right'
+      className: 'admin_mega-menu pull-right',
+
+      ui: {
+        'link': 'a.mega-link',
+        'megaChildMenu': '.mega-menu-content'
+      },
+
+      events: {
+        'click @ui.link': 'openChildMenu'
+      },
+
+      initialize: function () {
+
+        this.model = new User();
+        this.model.fetch();
+      },
+
+      modelEvents: {
+        'change': 'render'
+      },
+
+      openChildMenu: function (e) {
+        
+        this.ui.megaChildMenu.slideToggle('slow');
+      }
     });
   }
 );
